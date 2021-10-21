6d7134900d5d98cd046fc4c2b976cdb74a217698
Данила Грачев <danyagrachev@gmail.com>
2021-10-21 16:35:18 +0600

    Initial commit

---
+++ src/LR1/Main.java
@@ -0,0 +1,8 @@
+package LR1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("gasga");
+    }
+}
---
+++ src/LR2/Main.java
@@ -0,0 +1,7 @@
+package LR2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
