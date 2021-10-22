e73ca830778793f3e2a3be5ee851f45572c4666f
Данила Грачев <danyagrachev@gmail.com>
2021-10-22 14:47:52 +0600

    LR2 improvement

--- src/LR2/Main.java
+++ src/LR2/Main.java
@@ -9,47 +9,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Map<String, ArrayList<String>> classes = new HashMap<>();
-        Stream<Path> paths = Files.walk(Paths.get("src/LR2/input"));
-        paths.filter(Files::isRegularFile)
-                .map(Path::toString)
-                .filter(f -> f.endsWith(".java"))
-                .forEach(f -> {
-                    String data = "";
-                    try (Scanner in = new Scanner(new File(f))) {
-                        data = in.useDelimiter("\\A").next();
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    Pattern pattern = Pattern.compile("(class|interface) +([A-Za-z]\\w*) *(<\\w+>)? *" +
-                            "(extends +([A-Za-z]\\w*))? *(implements +([A-Za-z]\\w*))?", Pattern.MULTILINE);
-                    Matcher matcher = pattern.matcher(data);
-                    while (matcher.find()) {
-                        var name = matcher.group(2);
-                        classes.put(name, classes.getOrDefault(name, new ArrayList<>()));
-                        parentEntity op = (parent) -> {
-                            if (parent != null) {
-                                classes.put(parent, classes.getOrDefault(parent, new ArrayList<>()));
-                                var parentHeirs = classes.getOrDefault(parent, new ArrayList<>());
-                                parentHeirs.add(name);
+        Map<String, ArrayList<String>> entities = new HashMap<>();
+        try (Stream<Path> paths = Files.walk(Paths.get("src/LR2/input"))) {
+            paths.filter(Files::isRegularFile)
+                    .map(Path::toString)
+                    .filter(f -> f.endsWith(".java"))
+                    .forEach(f -> {
+                        StringBuilder data = new StringBuilder();
+                        try (Scanner in = new Scanner(new File(f))) {
+                            while (in.hasNextLine()) {
+                                data.append(in.nextLine()).append(" ");
                             }
-                        };
-                        var parentClass = matcher.group(5);
-                        var parentInterface = matcher.group(7);
-                        op.addHeirs(parentClass);
-                        op.addHeirs(parentInterface);
-                    }
-                });
-        System.out.println(classes);
-    }
-    interface parentEntity {
-        void addHeirs(String parent);
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        Pattern pattern = Pattern.compile("(class|interface) +([A-Za-z]\\w*) *(<\\w+>)? *" +
+                                "(extends +([A-Za-z]\\w*))? *(implements +([A-Za-z]\\w*))?", Pattern.MULTILINE);
+                        Matcher matcher = pattern.matcher(data.toString());
+                        while (matcher.find()) {
+                            var name = matcher.group(2);
+                            var parentClass = matcher.group(5);
+                            var parentInterface = matcher.group(7);
+                            entities.put(name, entities.getOrDefault(name, new ArrayList<>()));
+                            Consumer<String> action = (parent) -> {
+                                if (parent != null) {
+                                    entities.put(parent, entities.getOrDefault(parent, new ArrayList<>()));
+                                    var children = entities.getOrDefault(parent, new ArrayList<>());
+                                    children.add(name);
+                                }
+                            };
+                            action.accept(parentClass);
+                            action.accept(parentInterface);
+                        }
+                    });
+        }
+        System.out.println(entities);
     }
 }
